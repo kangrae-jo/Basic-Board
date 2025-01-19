c3d5defb6ded7007dc4161b781c3c3390ed210ed
kangrae <kangrae0501@gmail.com>
2025-01-19 17:26:30 +0900

    feat: 유저 엔티티 생성
    - 유저 엔티티는 id, title, content, author field를 기본적으로 가짐
    - 추가로 게시일, 수정일을 관리함
    - 엔티티 관리를 위한 gradle 의존성 추가

---
+++ src/main/java/core/basicboard/entity/Board.java
@@ -0,0 +1,33 @@
+package core.basicboard.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Board {
+
+    @Id // 기본키
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String author;
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime updateDate;
+}
